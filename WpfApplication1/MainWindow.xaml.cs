48c1656526263de6050b39080f7322666ef84543
Qchta <QchtaPL@gmail.com>
2016-04-08 14:26:09 +0200

    Rysowanie Kwadratów

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -20,9 +20,51 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
+        Point currentPoint;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void LiczPunkty()
+        {
+            
+        }
+
+        private void SingePlayerButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void PoleGry_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+                currentPoint = e.GetPosition(this);
+        }
+
+        private void PoleGry_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Rectangle point = new Rectangle();
+
+                SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+
+                mySolidColorBrush.Color = Color.FromArgb(255, 255, 255, 0);
+                point.Fill = mySolidColorBrush;
+                point.StrokeThickness = 0;
+
+                Canvas.SetTop(point, currentPoint.Y - currentPoint.Y%20);
+                Canvas.SetLeft(point, currentPoint.X - currentPoint.X % 20);
+
+                point.Width = 20;
+                point.Height = 20;
+
+                PoleGry.Children.Add(point);
+
+                currentPoint = e.GetPosition(this);
+            }
         }
     }
 }
